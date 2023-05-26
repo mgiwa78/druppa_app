e77ba44b6828a28c36d5e67582ca0df51d5086a4
Giwa Muhammad <mgiwa78@gmail.com>
2023-05-26 17:19:08 +0100

    Added Dist folder

---
+++ dist/assets/assets.239de799.js
@@ -0,0 +1,1 @@
+import{O as n,al as r}from"./index.9e5782ba.js";const a=s=>{const t=s.substring(s.lastIndexOf("."),s.length),e=n().mode=="dark"?`${s.substring(0,s.lastIndexOf("."))}-dark`:s.substring(0,s.lastIndexOf("."));return`./media/illustrations/${r.value}/${e}${t}`},d=s=>"./"+s;export{a,d as g};
