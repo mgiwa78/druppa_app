1d4818559fcbdc849d12964533f57be8b6653005
Giwa Muhammad <mgiwa78@gmail.com>
2023-09-16 15:46:04 +0100

    Create gh-pages branch with dist files

--- src/stores/logout.ts
+++
@@ -1,9 +0,0 @@
-import { useStaticsStore } from "./statics";
-import { useAuthStore } from "./auth";
-
-const auth = useAuthStore();
-const statics = useStaticsStore();
-export function purgeAuth() {
-  auth.$reset();
-  statics.$reset();
-}
